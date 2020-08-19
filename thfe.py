b3ab2407732b93bb950f0993add3d2205bf788b4
Valdigleis <valdigleis@Hal9000.local>
2020-08-18 21:58:27 -0300

    THFE complete

---
+++ thfe.py
@@ -0,0 +1,58 @@
+class THFE:
+
+    def __init__(self, values):
+        self.__values = set()
+        for x in values:
+            if x > 1.0:
+                self.__values.add(abs(1.0/x))
+            elif x < 0.0:
+                self.__values.add(abs(1.0/x))
+            else:
+                self.__values.add(x)
+
+    def getValues(self):
+        return self.__values
+
+    def normalize(self, n):
+        if len(self.__values) > n:
+            vector = sorted(list(self.__values))
+            T = len(vector)-1
+            nList = list()
+            for i in range(n):
+                nList.append(vector[T - i])
+            self.__values = set(nList)
+
+    def __add__(self, other):
+        result = set()
+        for x in self.__values:
+            for y in other.getValues():
+                result.add(max(x, y))
+        return THFE(result)
+
+    def __mul__(self, other):
+        result = set()
+        for x in self.__values:
+            for y in other.getValues():
+                result.add(min(x, y))
+        return THFE(result)
+
+    def __floordiv__(self, other):
+        """Metodo que implementa o calculo de distancia jaccard."""
+        U = self.__values.union(other.getValues())
+        I = self.__values.intersection(other.getValues())
+        return len(I)/len(U)
+
+    def __lshift__(self, other):
+        '''Metodo que implementa a distancia média'''
+        S = 0.0
+        for x in self.__degrees:
+            for z in other.getDegrees():
+                S = S + abs(x - z)
+        return Z/(len(self.__degrees) * len(other.getDegrees()))
+
+    def __str__(self):
+        return str(self.__values)
+
+
+ONE = THFE([1.0])
+ZERO = THFE([0.0])
